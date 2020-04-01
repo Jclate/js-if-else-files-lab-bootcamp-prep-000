3c9556950ce9530ed8705c814f642d2c0b310189
Jake Gonchar <jake098877@gmail.com>
2020-04-01 20:57:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ var height = "74"
 if (name === "Susan") {
     console.log("This is name correct")
 }
+else {
+  "expected name to be Susan"
+}
 
 // Don't worry about this
 module.exports = { name, height
